ccf058c720794572e065299c2e62ea99e6a0a675
Felix Moses <felix@tacocat.com>
2023-02-09 19:08:02 -0800

    new editor view pt 1

--- index2.js
+++ index2.js
@@ -1,14 +1,77 @@
 
 let nodes = [
     {
-        id: 0,
-        author: "Vincent",
-        text: "",
-        paths: [],
-        x: 25,
-        y: 25
+        "id": 0,
+        "author": "Vincent",
+        "text": "How are \nyou doing?",
+        "paths": [
+            "dedff9d0-12b2-466f-aeac-87c6cb028777",
+            "b8f1ccb0-581a-4079-998e-9b0c28b7f2b5"
+        ],
+        "x": 337.5,
+        "y": 62.5
+    },
+    {
+        "id": "dedff9d0-12b2-466f-aeac-87c6cb028777",
+        "author": "Phage",
+        "text": "Bla bla bla",
+        "paths": [
+            "b622b79e-49ed-4f6f-b661-014a573a42bc"
+        ],
+        "x": 262.5,
+        "y": 162.5
+    },
+    {
+        "id": "b8f1ccb0-581a-4079-998e-9b0c28b7f2b5",
+        "author": "Phage",
+        "text": "This is pretty cool",
+        "paths": [
+            "6df41bbb-98b4-46db-9a6a-ba96110c22ed"
+        ],
+        "x": 412.5,
+        "y": 162.5
+    },
+    {
+        "id": "b622b79e-49ed-4f6f-b661-014a573a42bc",
+        "author": "Vincent",
+        "text": "How dare you!",
+        "paths": [
+            "614fa322-5a4d-4645-9b30-e27318722ffa"
+        ],
+        "x": 262.5,
+        "y": 262.5
+    },
+    {
+        "id": "6df41bbb-98b4-46db-9a6a-ba96110c22ed",
+        "author": "Vincent",
+        "text": "No, its not",
+        "paths": [
+            "614fa322-5a4d-4645-9b30-e27318722ffa"
+        ],
+        "x": 412.5,
+        "y": 262.5
+    },
+    {
+        "id": "614fa322-5a4d-4645-9b30-e27318722ffa",
+        "author": "Phage",
+        "text": "Hell ya",
+        "paths": [
+            "f049c3de-6c7d-42b0-989d-af23cf237532"
+        ],
+        "x": 337.5,
+        "y": 362.5
+    },
+    {
+        "id": "f049c3de-6c7d-42b0-989d-af23cf237532",
+        "author": "Vincent",
+        "text": "Good by",
+        "paths": [],
+        "x": 337.5,
+        "y": 462.5
     }
 ]
+
+let player = "Phage"
 
 const graph = document.querySelector("#graph")
 
@@ -161,31 +224,106 @@ function NodeView({ node }) {
 
 //
 
+function BackAreaView() {
+    const view = document.createElement("div")
+    view.className = "back"
+    view.innerHTML = "< back"
+    view.onclick = goToParent
+    return view
+}
+
+function AuthorView(node) {
+    const view = document.createElement("input")
+    view.className = "author"
+    view.value = node.author
+    view.oninput = () => {
+        node.author = view.value
+        update()
+    }
+    return view
+}
+
+function TextView(node) {
+    const view = document.createElement("div")
+    view.className = "text"
+    view.innerHTML = node.text
+    view.contentEditable = true
+
+    view.oninput = () => {
+        // save the new value
+        node.text = getInnerHtml(view)
+    
+        // update
+        update()
+    }
+    
+    return view
+}
+
+function getInnerHtml(view) {
+    // https://stephenhaney.com/2020/get-contenteditable-plaintext-with-correct-linebreaks
+
+    let newValue = ""
+    let isOnFreshLine = true
+
+    function parseChildNodesForValueAndLines(childNodes) {
+        for (let i = 0; i < childNodes.length; i++) {
+            const childNode = childNodes[i];
+            if (childNode.nodeName === 'BR') {
+                newValue += '\n';
+                isOnFreshLine = true;
+                continue;
+            }
+            if (childNode.nodeName === 'DIV' && isOnFreshLine === false) {
+                newValue += '\n';
+            }
+            isOnFreshLine = false;
+            if (childNode.nodeType === 3 && childNode.textContent) {
+                newValue += childNode.textContent;
+            }
+            parseChildNodesForValueAndLines(childNode.childNodes);
+        }
+    }
+
+    parseChildNodesForValueAndLines(view.childNodes)
+
+    return newValue
+}
+
+function OptionView(i, node) {
+    const view = document.createElement("div")
+    view.className = "option"
+    view.innerHTML = `${i}. "${node.text}"`
+    view.onclick = () => select(node)
+    return view
+}
+
 function select(node) {
     // select element
     selected = node.id
 
-    // set values
-    document.getElementById("author").value = node.author
-    document.getElementById("body").value = node.text
-
-    // expand textarea
-    bodyView.style.height = "0px"
-    bodyView.style.height = bodyView.scrollHeight + "px"
-
     // add paths
-    function* getChildren() {
-        for (const id of node.paths) {
-            const doc = document.createElement("div")
-            doc.className = "box"
-            doc.innerHTML = `<b>${getNode(id).author}</b>\n${getNode(id).text}`
-            doc.onclick = () => {
-                select(getNode(id))
+    function* getChildren(node) {
+        let paths = node.paths
+
+        if (paths.length == 1 && getNode(paths[0]).author !== player) {
+            yield AuthorView(getNode(paths[0]))
+            yield TextView(getNode(paths[0]))
+
+            yield *getChildren(getNode(paths[0]))
+        } else {
+            for (const i in paths) {
+                yield OptionView(Number(i) + 1, getNode(paths[i]))
             }
-            yield doc
         }
     }
-    document.getElementById("paths").replaceChildren(...getChildren())
+
+    document.getElementById("editor").replaceChildren(
+        BackAreaView(),
+        AuthorView(node),
+        TextView(node),
+        ...getChildren(node)
+    )
 
     // update graph
     update()
@@ -193,23 +331,7 @@ function select(node) {
     document.getElementById(node.id).focus()
 }
 
-const bodyView = document.getElementById("body");
-bodyView.oninput = (e) => {
-    // save the new value
-    getNode(selected).text = bodyView.value
-
-    // expand it
-    bodyView.style.height = "0px"
-    bodyView.style.height = bodyView.scrollHeight + "px"
-
-    // update
-    update()
-}
-
-document.getElementById("author").oninput = (e) => {
-    getNode(selected).author = document.getElementById("author").value
-    update()
-}
+//
 
 function getNode(id) {
     return nodes.find(node => node.id == id)
@@ -232,10 +354,10 @@ function update() {
     }
 }
 
+//
 
 const mouse = {}
 const camera = { x: 0, y: 0 }
-
 
 graph.onmousedown = (e) => {
     mouse.isDown = true
